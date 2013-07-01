ab6778cbfe3c4cb12367adf598d5f76e1bd21eca
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2013-07-01 13:17:41 +0200

    pinger code + readme

---
+++ README.md
@@ -0,0 +1,24 @@
+WebsitePinger
+=============
+
+This is a simple project to ping websites, it is really useful for websites hosted on IIS servers with low traffic, thinking mainly on my demo projects hosted on **AppHarbor**.
+
+Pinging your website will make the page load much faster for other visitors as starting the worker process on AppHarbor takes its time.
+
+This project can be forked and you can host it in your home server or at AppHarbor. Also there are a few improvements that can be done, listed on the TODO list at the end of the document.
+
+Steps:
+1. Fork the project
+2. Create or log in into your AppHarbor account
+3. Create a new application
+4. Connect it to your repository
+5. Modify the url list with your own and push the changes to your repository, AppHarbor will then deploy your last commit
+
+That's all!
+
+
+##TODO
+
+* Log data, use NLog and config Logentries in web.config
+* Get feedback on offline urls
+
---
+++ WebsitePinger/WebsitePinger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+
+namespace WebsitePinger
+{
+    class WebsitePinger
+    {
+        public WebsitePinger()
+        {
+            string[] urls = new string[] {
+                "http://raquelestrada.apphb.com/",
+                "http://wmhomepage.apphb.com/",
+                "http://twitter-stats.apphb.com/"
+
+            };
+
+            try
+            {
+                var client = new WebClient();
+                foreach (var url in urls)
+                {
+                    client.DownloadData(new Uri(url));
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine("Error pinging websites: ", ex.Message);
+                // TODO: Log to logentries
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            new WebsitePinger();
+
+            return 1; // return code != 0 to re-execute
+        }
+    }
+}
