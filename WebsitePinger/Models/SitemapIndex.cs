e1b5f5aa954417d22b7bcee20c573d21068c539e
Ferran Salguero <fsalgueromateu@gmail.com>
2014-10-17 20:00:53 +0200

    sitemap index class

---
+++ WebsitePinger/Models/SitemapIndex.cs
@@ -0,0 +1,25 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace WebsitePinger.Models
+{
+    [XmlRoot("sitemapindex", Namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")]
+    public class SitemapIndex
+    {
+        [XmlAttribute("xmlns")]
+        public string Xmlns { get; set; }
+
+        [XmlElement("sitemap")]
+        public List<Sitemap> Sitemaps { get; set; }
+    }
+
+    public class Sitemap
+    {
+        [XmlElement("loc")]
+        public string loc { get; set; }
+    }
+}
