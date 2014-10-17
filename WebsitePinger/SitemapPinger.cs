01569d539dbf7466f594ac1b84635f763cc8ca3c
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2014-10-17 14:26:39 +0200

    sitemap pinger idea

---
+++ WebsitePinger/SitemapPinger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+using WebsitePinger.Models;
+
+namespace WebsitePinger
+{
+    public class SitemapPinger
+    {
+        public SitemapPinger()
+        {
+            string[] urls = new string[] {
+                "http://whereshouldibuy.apphb.com/sitemap/Index",
+            };
+
+            using (var client = new WebClient())
+            {
+                client.Proxy = new WebProxy("http://10.49.1.1:8080");
+                foreach (var url in urls)
+                {
+                    //var response = client.DownloadString(url);
+
+                    using (var reader = client.OpenRead(url))
+                    {
+                        var s = new XmlSerializer(typeof(Sitemap[]));
+                        var index = (SitemapIndex)s.Deserialize(reader);
+
+                        Console.WriteLine("num sitemaps -> " + index.Sitemaps.Length);
+
+                    }
+                }
+
+            }
+        }
+    }
+}
